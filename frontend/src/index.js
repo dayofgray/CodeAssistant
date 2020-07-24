c51dcbc3999271472bc9658a964e4044fa80cc67
dayofgray <graysonpberger@gmail.com>
2020-07-24 17:42:33 -0400

    Add card-container wrapper and back button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-
+let cardContainer
 init()
 function init(){
   getLanguages()
@@ -6,35 +6,42 @@ function init(){
 
 function getLanguages() {
 const main = document.getElementsByTagName("main")[0]
-main.innerHTML = null
+if (!cardContainer) {
+  main.appendChild(document.createElement("div")).setAttribute("class", "card-container")
+  cardContainer = document.getElementsByClassName("card-container")[0]
+}
+cardContainer.innerHTML = null
 ApiAdapter.get('/languages')
  .then( languages => {
    languages.forEach(lang => {
      const l = new Language(lang)
      let div = l.renderCard()
      div.addEventListener("click", moveToNotes)
-     main.appendChild(div)
+     cardContainer.appendChild(div)
    })
-   main.appendChild(Language.renderAddLanguage())
+   cardContainer.appendChild(Language.renderAddLanguage())
    const form = document.getElementById("create-language")
    form.addEventListener("submit", Language.submitLanguage)
    })
  }
 
 function moveToNotes(e, id) {
-  console.log(e)
-  console.log(e)
   const lang_id = id ? id : e.target.getAttribute("data-id")
-  const main = document.getElementsByTagName("main")[0]
-  main.innerHTML = null
+  cardContainer.innerHTML = null
+  const p = document.createElement("p")
+  const a = document.createElement("a")
+  a.href = "index.html"
+  cardContainer.appendChild(a)
   ApiAdapter.get(`/languages/${lang_id}`)
     .then(language => {
       language.notes.forEach(note => {
         const n = new Note(note)
-        main.appendChild(n.renderCard())
+        cardContainer.appendChild(n.renderCard())
       })
-      main.appendChild(Note.renderAddNote(lang_id))
+      cardContainer.appendChild(Note.renderAddNote(lang_id))
       const form = document.getElementById("create-note")
       form.addEventListener("submit", Note.submitNote)
     })
+    const back = document.getElementById("back")
+    back.toggleAttribute("hidden")
 }
