027049a66d68217310ef7c41e458ab9c39e57aa6
dayofgray <graysonpberger@gmail.com>
2020-08-04 10:51:22 -0400

    Add sort initial from exercise

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,44 @@ function init(){
   getLanguages()
 }
 
+let sort = document.getElementById("sort")
+sort.addEventListener("click", sortCards)
+
+function sortCards() {
+  fetch("http://localhost:3000/languages")
+  .then(resp => resp.json())
+  .then(json => {
+    json.sort(function(a, b) {
+    var titleA = a.title.toUpperCase(); // ignore upper and lowercase
+    var titleB = b.title.toUpperCase(); // ignore upper and lowercase
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1;
+    }
+  
+    // names must be equal
+    return 0;
+  })
+  cardContainer.innerHTML = null
+  json.forEach(lang => {
+     const l = new Language(lang)
+     let div = l.renderCard()
+     let title = div.getElementsByTagName("p")[0]
+     title.addEventListener("click", moveToNotes)
+     cardContainer.appendChild(div)
+     let button = div.getElementsByTagName("button")[0]
+     button.addEventListener("click", l.deleteLanguage.bind(l))
+    })
+  cardContainer.appendChild(Language.renderAddLanguage())
+
+  const form = document.getElementById("create-language")
+  form.addEventListener("submit", Language.submitLanguage)
+  })
+}
+
+
 function getLanguages() {
 const main = document.getElementsByTagName("main")[0]
 if (!cardContainer) {
@@ -27,6 +65,7 @@ ApiAdapter.get('/languages')
    form.addEventListener("submit", Language.submitLanguage)
    })
  }
+
 
 function moveToNotes(e, id) {
   const lang_id = id ? id : e.target.parentNode.getAttribute("data-id")
